852657976fffa75198ce74449c06d09ac9244851
kenzo <kenzou.kenzou104809@gmail.com>
2020-11-08 17:05:07 +0900

    fix indent

--- abc001/c.rb
+++ abc001/c.rb
@@ -1,12 +1,12 @@
 N = gets.to_i
 ary = []
 N.times do |n|
-    ary << gets.split.map(&:to_i)
+  ary << gets.split.map(&:to_i)
 end
 ary.combination(3) do |a|
-    if (a[1][0] - a[0][0]) *  (a[2][1] - a[1][1])  == (a[2][0] - a[1][0]) * (a[1][1] - a[0][1])
-        puts 'Yes'
-        exit
-    end
+  if (a[1][0] - a[0][0]) *  (a[2][1] - a[1][1])  == (a[2][0] - a[1][0]) * (a[1][1] - a[0][1])
+    puts 'Yes'
+    exit
+  end
 end
 puts 'No'
