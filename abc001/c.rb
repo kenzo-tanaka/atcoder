98781a34df289c27abb64663ca7f748190414267
kenzo <kenzou.kenzou104809@gmail.com>
2020-11-08 17:01:02 +0900

    fix c

--- abc001/c.rb
+++ abc001/c.rb
@@ -1,41 +1,12 @@
-# input sample
-# 4
-# 0 1
-# 0 2
-# 0 3
-# 1 1
-
-N = gets.chomp.to_i
-xs = [] # x座標
-ys = [] # y座標
-ts = [] # 傾き
-
-N.times do
-  x, y = gets.split(' ').map(&:to_i)
-  xs << x
-  ys << y
-  ts << (y-x)
+N = gets.to_i
+ary = []
+N.times do |n|
+    ary << gets.split.map(&:to_i)
 end
-
-xs.uniq.map do |e| 
-  if xs.count(e) >= 3
-    p 'Yes'
-    exit
-  end
+ary.combination(3) do |a|
+    if (a[1][0] - a[0][0]) *  (a[2][1] - a[1][1])  == (a[2][0] - a[1][0]) * (a[1][1] - a[0][1])
+        puts 'Yes'
+        exit
+    end
 end
-
-ys.uniq.map do |e| 
-  if ys.count(e) >= 3
-    p 'Yes'
-    exit
-  end
-end
-
-ts.uniq.map do |e| 
-  if ts.count(e) >= 3
-    p 'Yes'
-    exit
-  end
-end
-
-p 'No'
+puts 'No'
